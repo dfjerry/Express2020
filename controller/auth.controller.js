07fc18d94df12b6c050466de8dc6f4784e37972a
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-04-28 15:12:33 +0700

    update

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -7,21 +7,21 @@ module.exports.login = function (req, res) {//req: request, res: response
 module.exports.postLogin = function (req, res) {
     var email = req.body.email; // lay email o post request
     var password = req.body.password;
-    var count = 0;
-    for(var i = 0; i < email.length; i++){
-        if(email[i] === '@'){
-            count ++;
-        }
-    }
-    if(count !== 1 ){
-        res.render('auth/login', {//cho ng dung quay tro lai trang login
-            errors: [
-                'Email chỉ cần và chỉ có 1 ký tự @!!'//tao 1 obj errors de ti in ra loi
-            ],
-            values: req.body //de ng dung dien gi thi van luu o input
-        });
-        return;// return de dung lai
-    }
+    // var count = 0;
+    // for(var i = 0; i < email.length; i++){
+    //     if(email[i] === '@'){
+    //         count ++;
+    //     }
+    // }
+    // if(count !== 1 ){
+    //     res.render('auth/login', {//cho ng dung quay tro lai trang login
+    //         errors: [
+    //             'Email chỉ cần và chỉ có 1 ký tự @!!'//tao 1 obj errors de ti in ra loi
+    //         ],
+    //         values: req.body //de ng dung dien gi thi van luu o input
+    //     });
+    //     return;// return de dung lai
+    // }
     var user = db.get('users').find({email: email}).value();//tim trong db co email trung voi email o tren no se ra value la 1 obj cua users do
 
     if(!user) {//neu ko tim thay trong db thi users ko ton tai
@@ -34,7 +34,8 @@ module.exports.postLogin = function (req, res) {
         return;// return de dung lai
     }
     //neu co user
-    if(password !== user.password){
+    var hassedPassword = md5(password);
+    if( user.password !== hassedPassword){
         res.render('auth/login', {
             errors: [
                 'Wrong password!!'//tao 1 obj errors de ti in ra loi
@@ -44,7 +45,9 @@ module.exports.postLogin = function (req, res) {
         return;
     }
     //trc khi redirect sang users thi set 1 cai cookie
-    res.cookie('usersId', user.id);//set cookie name: usersId, value la user.id
+    res.cookie('usersId', user.id, {
+        singed: true
+    });//set cookie name: usersId, value la user.id
     res.redirect('/users');
 };
 
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,15 +2,16 @@ var db = require('../db');
 
 module.exports.requireAuth = function (req, res, next) {
     //check xem cookie co dc gui len ko
-    if(!req.cookies.userId){//neu co cookie la userId thi cho qua, thuc te thi ko ai lam the nay, chi la demo hoat dong cookie
+    if(!req.signedCookies.userId){//neu co cookie la userId thi cho qua, thuc te thi ko ai lam the nay, chi la demo hoat dong cookie
         res.redirect('/auth/login');//cho quay lai login
         return;//return ko thi no se chay login ben duoi
     }
-    var user = db.get('users').find({id : req.cookies.userId}).value();//kiem tra trong db xem userId co = id ko
+    var user = db.get('users').find({id : req.signedCookies.userId}).value();//kiem tra trong db xem userId co = id ko
     if(!user){
         res.redirect('/auth/login');//cho quay lai login
         return;//return ko thi no se chay login ben duoi
     }
+    res.locals.user = user;//gan user vao bien local
     //check het ma ok thi cho next
     next();
 };
